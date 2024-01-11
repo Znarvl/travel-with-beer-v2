b03a1e9765d25e289ca4f85d0abba571edaae709
Simon Jakobsson <31953373+Znarvl@users.noreply.github.com>
2024-01-11 13:59:48 +0100

    trying this again

--- __tests__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render, screen } from "@testing-library/react"
-import Home from "@/app/page"
-
-it("should have docs text", () => {
-    render(<Home/>)
-    const myElem = screen.getByText('Docs')
-    expect(myElem).toBeInTheDocument()
-})
